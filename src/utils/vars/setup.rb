e141dbd15b29f34b5558b46f716d587b6aa0a84f
Alessandro Molari <molari.alessandro@gmail.com>
2016-04-06 08:25:15 +0000

    Split vars into multiple files

---
+++ src/utils/vars/setup.rb
@@ -0,0 +1,88 @@
+module Fizzy::Vars
+  class Setup
+
+    include Fizzy::IO
+    include Fizzy::Filesystem
+
+    def initialize(vars_dir_path, name)
+      @vars_dir_path = vars_dir_path
+      @name          = name
+    end
+
+    # Setup the variables that will be used during ERB processing.
+    #
+    # Those variables will be set into an instance field called `@vars`.
+    #
+    # After calling this method, you can directly access the variables using
+    # `@vars` or using the attribute reader `vars`.
+    #
+    def run
+      setup_vars(@vars_dir_path, @name)
+    end
+
+  protected
+
+    def setup_vars(vars_dir_path, name)
+      fmt, content = read_vars(vars_dir_path, name)
+      error("Invalid vars: `#{name}`.") if fmt == nil || content == nil
+      self_vars    = parse_vars(fmt, content)
+      parents      = parse_parents_vars(fmt, content)
+      parents_vars = merge_parents_vars(vars_dir_path, parents)
+      merge_with_parents_vars(self_vars, parents_vars)
+    end
+
+    def read_vars(vars_dir_path, name)
+      yaml_file_path = find_yaml_path(vars_dir_path.join(name)) unless vars_dir_path.nil? || name.nil?
+
+      if yaml_file_path
+        [:yaml, File.read(yaml_file_path)]
+      elsif !name.nil? && ENV.has_key?(name)
+        [:json, ENV[name]]
+      else
+        [nil, nil]
+      end
+    end
+
+    def parse_vars(fmt, content)
+      case fmt
+      when :yaml then YAML.load(content) || {}
+      when :json
+        begin
+          JSON.parse(content)
+        rescue
+          error("Invalid JSON: `#{content}`.")
+        end
+      else error("Unrecognized format: `#{fmt}`")
+      end
+    end
+
+    def parse_parents_vars(fmt, content)
+      parents_regexp = case fmt
+                       when :yaml then Fizzy::CFG.vars.yaml_regexp
+                       when :json then Fizzy::CFG.vars.json_regexp
+                       else error("Unrecognized format: `#{fmt}`.")
+                       end
+      if md = content.match(parents_regexp)
+        md[:parents].split(",")
+                    .map(&:strip)
+                    .reject{|p| p =~ Fizzy::CFG.vars.parent_dummy_regexp}
+      else
+        []
+      end
+    end
+
+    def merge_with_parents_vars(self_vars, parents_vars)
+      parents_vars.deep_merge(self_vars)
+    end
+
+    def merge_parents_vars(vars_dir_path, parents)
+      parents.inject([]) do |acc, parent| # Vars for each parent.
+        parent_vars = setup_vars(vars_dir_path, parent)
+        acc << parent_vars
+      end.inject({}) do |acc, parent_vars| # Merge them.
+        acc.deep_merge(parent_vars)
+      end
+    end
+
+  end
+end

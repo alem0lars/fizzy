a37eb1e54d0816285bba8b530163fbc6a29f77e7
Alessandro Molari <molari.alessandro@gmail.com>
2016-06-13 18:58:10 +0200

    Initial changes for `sync` infrastructure

--- src/core/sync/local.rb
+++ src/core/sync/local.rb
@@ -2,6 +2,37 @@ class Fizzy::Sync::Local < Fizzy::Sync::Base
 
   def initialize(local_dir_path, remote_url)
     super
+    @remote_path = Pathname.new(@remote_url)
+  end
+
+  # Check if the synchronizer is enabled.
+  #
+  def enabled?
+    @remote_path.directory? || super
+  end
+
+  # Update local from the remote.
+  #
+  def update_local
+    # TODO
+  end
+
+  # Update remote from local.
+  #
+  def update_remote
+    # TODO
+  end
+
+  # Check if local is changed, and now is different from latest remote state.
+  #
+  def local_changed?
+    # TODO
+  end
+
+  # Check if remote is changed, and now is different from latest local state.
+  #
+  def remote_changed?
+    # TODO
   end
 
 end
--- test/test_sync.rb
+++ test/test_sync.rb
@@ -2,13 +2,45 @@ require "helper"
 
 describe Fizzy::Sync do
 
-  before do
-    @local_dir_path = "foo"
-    @remote_url = "github.com/cingli"
+  let :running_in_docker do
+    system("awk -F/ '$2 == \"docker\"' /proc/self/cgroup 2&>/dev/null | read")
   end
 
-  it "asdasd" do
-    puts Fizzy::Sync.available_synchronizers(@local_dir_path, @remote_url)
+  describe "#available" do
+    it "should contain available synchronizers" do
+      synchronizers = Fizzy::Sync.available
+      synchronizers.must_be_kind_of Array
+      synchronizers.wont_be_empty
+    end
+  end
+
+  describe "#enabled" do
+    it "should contain enabled synchronizers" do
+      # When remote is `git:`, `Fizzy::Sync::Git` is enabled.
+      syncs = Fizzy::Sync.enabled(Pathname.new("foo"), "git:bar")
+      syncs.length.must_be :==, 1
+      syncs.first.must_be_kind_of Fizzy::Sync::Git
+
+      # When remote is an existing directory, `Fizzy::Sync::Local` is enabled.
+      Dir.mktmpdir("foo") do |dir_path|
+        syncs = Fizzy::Sync.enabled(Pathname.new("foo"), dir_path)
+        syncs.any?{|e| e.is_a? Fizzy::Sync::Local}.must_equal true
+      end
+    end
+  end
+
+  describe "#selected" do
+    it do
+      %w(git:foobar foobar).each do |remote_url|
+        sync = Fizzy::Sync.selected(Pathname.new("foo"), remote_url)
+        sync.must_be_kind_of Fizzy::Sync::Git
+      end
+
+      Dir.mktmpdir("foo") do |dir_path|
+        sync = Fizzy::Sync.selected(Pathname.new("foo"), dir_path)
+        sync.must_be_kind_of Fizzy::Sync::Local
+      end
+    end
   end
 
 end

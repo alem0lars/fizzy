7ca877dfc76b63f3e90a79c1cdd6165a12a6bd3b
Alessandro Molari <molari.alessandro@gmail.com>
2016-10-04 17:39:37 +0200

    Update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -78,7 +78,7 @@ RSpec.configure do |config|
     config.add_formatter "Fuubar"
   end
 
-  config.add_formatter "html", "website/source/docs/test_results.html"
+  config.add_formatter "html", "website/source/docs/test-results.html"
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.

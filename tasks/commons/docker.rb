34fd8d1bbfa3d8edf705a35ac8ab3d416fc5b738
Alessandro Molari <alessandro.molari@yoroi.company>
2016-10-08 03:22:37 +0200

    move docker to dedicate directory

--- tasks/commons/docker.rb
+++ tasks/commons/docker.rb
@@ -3,9 +3,10 @@ def docker_image?(image_name)
 end
 
 def docker_build(image_name)
-  sh "docker build -t #{image_name} ."
+  dockerfile_path = $cfg[:paths][:docker].join("Dockerfile")
+  sh "docker build -f #{dockerfile_path} -t #{image_name} .", verbose: false
 end
 
 def docker_run(image_name, cmd)
-  sh "docker run -i -t #{image_name} #{cmd}"
+  sh "docker run -i -t #{image_name} #{cmd}", verbose: false
 end

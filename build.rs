16db6d870445ff637ecf65dea2856281e86ec785
alem0lars-svcuser <alem0lars.svcuser@gmail.com>
2017-02-10 17:39:48 +0100

    update

--- build.rs
+++ build.rs
@@ -1,5 +1,33 @@
 extern crate skeptic;
 
+extern crate clog;
+
+
+use clog::Clog;
+
+
 fn main() {
+    run_skeptic();
+    run_clog();
+}
+
+fn run_skeptic() {
     skeptic::generate_doc_tests(&["README.md"]);
+}
+
+fn run_clog() {
+    // Create the struct
+    let mut clog = Clog::with_dir("~/clog").unwrap_or_else(|e| {
+        // Prints the error message and exits
+        e.exit();
+    });
+
+    // TODO
+    clog.repository("https://github.com/alem0lars/fizzy")
+        .subtitle("fizzy changelog")
+        .changelog("CHANGELOG.md")
+        .version("0.1.0");
+
+    // Write the changelog to the current working directory.
+    clog.write_changelog().unwrap_or_else(|e| { e.exit(); });
 }

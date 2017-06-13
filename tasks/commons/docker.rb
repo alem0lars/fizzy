f069c189fce12df18e0a7684affba2d98453adcb
Alessandro Molari <molari.alessandro@gmail.com>
2017-06-13 23:00:02 +0200

    fix(dockerfile)

--- tasks/commons/docker.rb
+++ tasks/commons/docker.rb
@@ -8,5 +8,5 @@ def docker_build(image_name)
 end
 
 def docker_run(image_name, cmd)
-  sh "docker run -i -t #{image_name} #{cmd}", verbose: false
+  sh "docker run -it #{image_name} #{cmd}", verbose: false
 end

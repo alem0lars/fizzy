c342b13223e557327802489b31989b2dcbf45883
Alessandro Molari <molari.alessandro@gmail.com>
2017-06-14 07:14:10 +0200

    fix(docker): fixed docker image and commands

--- tasks/commons/docker.rb
+++ tasks/commons/docker.rb
@@ -2,11 +2,20 @@ def docker_image?(image_name)
   !`docker images -q #{image_name}`.strip.empty?
 end
 
+def docker_container?(container_name)
+  !`docker ps -q -f name=#{container_name}`.strip.empty?
+end
+
 def docker_build(image_name)
   dockerfile_path = $cfg[:paths][:docker].join("Dockerfile")
-  sh "docker build -f #{dockerfile_path} -t #{image_name} .", verbose: false
+  sh "docker build -f #{dockerfile_path} -t #{image_name} .", verbose: true
 end
 
 def docker_run(image_name, cmd)
-  sh "docker run -it #{image_name} #{cmd}", verbose: false
+  container_name = image_name
+  unless docker_container?(container_name)
+    sh "docker run --rm --name #{container_name} -d -it #{image_name}", verbose: true
+  end
+
+  sh "docker exec -it #{container_name} #{cmd}", verbose: true
 end
